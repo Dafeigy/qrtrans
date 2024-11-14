9e69dcb5ddbd20ca8641447a348d7e3f1f4c3dbf
Dafeigy <1070642765@qq.com>
2024-11-14 20:00:52 +0800

    <Update>: Central display

--- test.py
+++ test.py
@@ -14,18 +14,21 @@ Complex is better than complicated.
 Flat is better than nested.
 """.split()
 import time
+import os
+c, l = os.get_terminal_size()
+print(c)
 i = 0 
 
 while True:
 
     each = data[i]
     print("\033[H\033[J")
-    print("\n" * 5)
     qr.add_data(each)
     qr_matrix = qr.get_matrix()
     matrix = [["██" if each else "  " for each in rows] for rows in qr_matrix]
     for row in matrix:
-        print(" "*20 + "".join(row))
+        print("".join(row).center(c))
     qr.clear()
-    time.sleep(0.3)
+    time.sleep(0.06)
+    print('\r',end = '')
     i = (i + 1) % len(data)
